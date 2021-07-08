93d4833b1a119865c595375d74cdd8c6dbee720f
siddheshdeo1 <87115909+siddheshdeo1@users.noreply.github.com>
2021-07-08 14:28:34 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,33 @@
+
+#add header
+def add(x,y):
+    return x+y
+    
+#sub header
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    return x*y
+
+#div header
+def divide(x,y):
+<<<<<<< HEAD
+    return x//y
+=======
+
+    if y==0
+      return 0
+    return 11
+>>>>>>> 90fb845 (divide fun with erroro code)
+
+
+#changed
+
+#added by branch
+
+
+<<<<<<< HEAD
+#new line 123
+=======
+#merge in master
+>>>>>>> b123
